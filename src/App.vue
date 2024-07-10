38dbca9e516528b06402fd5a24b9ff379a49e5cd
murreletfeather <1941804585@qq.com>
2024-07-10 18:48:12 +0800

    使用bootstrap构建导航栏，还引入了一个@popperjs/core依赖

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <nav>
-
-  </nav>
-  <router-view/>
+  <NavBar />
+  <router-view />
 </template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+import NavBar from './components/NavBar';
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  }
+}
+</script>
+
+
 
 <style>
 #app {
