670a6b6b3d00221d112e5641bd43fcc0608328cb
murreletfeather <1941804585@qq.com>
2024-07-11 10:38:22 +0800

    页面路由,NavBar文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
 import UserList from '../views/UserListView.vue';
 import UserProfile from '../views/UserProfileView.vue';
@@ -42,7 +42,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
